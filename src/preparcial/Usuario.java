9e2c7b35dc53baf0661799876a5473da50657fa1
ian <somnii006@gmail.com>
2024-05-21 00:38:42 -0300

    no se como avanzar , te espero

--- src/preparcial/Usuario.java
+++ src/preparcial/Usuario.java
@@ -1,4 +1,42 @@
 package preparcial;
 
 public class Usuario extends Persona{
+    private String obraSocial;
+    private Recurso[] recurso;
+    private double facturaUsuario;
+    public Usuario(){
+
+    }
+
+    public Usuario(String apellido, long DNI, String nombre, String obraSocial, Recurso[] recurso, double facturaUsuario) {
+        super(apellido, DNI, nombre);
+        this.obraSocial = obraSocial;
+        this.recurso = recurso;
+        this.facturaUsuario = facturaUsuario;
+    }
+
+    public String getObraSocial() {
+        return obraSocial;
+    }
+
+    public void setObraSocial(String obraSocial) {
+        this.obraSocial = obraSocial;
+    }
+
+    public Recurso[] getRecurso() {
+        return recurso;
+    }
+
+    public void setRecurso(Recurso[] recurso) {
+        this.recurso = recurso;
+    }
+
+    public double getFacturaUsuario() {
+        return facturaUsuario;
+    }
+
+    public void setFacturaUsuario(double facturaUsuario) {
+        this.facturaUsuario = facturaUsuario;
+    }
+
 }
